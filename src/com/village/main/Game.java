9c2d7d5f0c8914f1ba17c8ecb93e3e4de1d0ffff
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2017-12-30 06:33:31 -0500

    Build path configured

--- src/com/village/main/Game.java
+++ src/com/village/main/Game.java
@@ -1,5 +1,28 @@
 package com.village.main;
 
-public class Game {
+import com.badlogic.gdx.ApplicationListener;
+
+public class Game implements ApplicationListener{
+
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void render() {
+		
+	}
+	
+	public void dispose() {
+		
+	}
+
+	
+	
+	public void pause() {}
+
+	public void resize(int arg0, int arg1) {}
+
+	public void resume() {}
 
 }
--- src/com/village/main/Main.java
+++ src/com/village/main/Main.java
@@ -1,9 +1,25 @@
 package com.village.main;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class Main {
 
+	public static final int WIDTH = 1024;
+	public static final int HEIGHT = 576;
+
 	public static void main(String[] args) {
+		Game game = new Game();
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		
+		
+		config.height = Main.HEIGHT;
+		config.width = Main.WIDTH;
+		config.resizable = false;
+
+        new LwjglApplication( game , config);
 	}
+	
+
 	
 }
